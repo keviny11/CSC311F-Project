a3a9993acdbba18682c339a8f1a75ec0bae03a28
张璐 <zhanglu@zhanglus-MacBook-Pro.local>
2020-11-12 20:09:49 -0500

    change knn# Changes to be committed:

--- part_a/knn.py
+++ part_a/knn.py
@@ -37,7 +37,11 @@ def knn_impute_by_item(matrix, valid_data, k):
     # TODO:                                                             #
     # Implement the function as described in the docstring.             #
     #####################################################################
-    acc = None
+    nbrs = KNNImputer(n_neighbors=k)
+    # We use NaN-Euclidean distance measure.
+    mat = nbrs.fit_transform(matrix.T)
+    acc = sparse_matrix_evaluate(valid_data, mat.T)
+    print("Validation Accuracy: {}".format(acc))
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
@@ -60,11 +64,23 @@ def main():
     # the best performance and report the test accuracy with the        #
     # chosen k*.                                                        #
     #####################################################################
-    pass
+    k=[1,6,11,16,21,26]
+    acc_user = []
+    acc_item = []
+    for i in range(len(k)):
+        acc1 = knn_impute_by_user(sparse_matrix,val_data,k[i])
+        acc_user.append(acc1)
+        acc2 = knn_impute_by_item(sparse_matrix,val_data,k[i])
+        acc_item.append(acc2)
+    # choose k* by getting the index of maximum accuracy in the list
+    acc_user_test = knn_impute_by_user(sparse_matrix,test_data,k[acc_user.index(max(acc_user))])
+    acc_item_test = knn_impute_by_item(sparse_matrix, test_data, k[acc_item.index(max(acc_item))])
+    return acc_user, acc_item, acc_user_test, acc_item_test
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
 
 
 if __name__ == "__main__":
-    main()
+    acc_user, acc_item, acc_user_test, acc_item_test = main()
+
